ae57f6edce92016dffeaf78cdac8ced147d010ad
Robertokello11 <robert.otieno@student.moringaschool.com>
2021-04-10 10:37:44 +0300

    add modifications

---
+++ script.js
@@ -0,0 +1,3 @@
+var dayNames = ["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var MaleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku", "Yaw","Kofi","Kwame"];
+var femaleNames =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
